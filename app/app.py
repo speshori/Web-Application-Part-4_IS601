719fcc74b212dc5bb32bbd82595bc875cefa6940
speshori <sap229@njit.edu>
2020-11-22 21:31:59 -0500

    Finished Edit and Delete

--- app/app.py
+++ app/app.py
@@ -33,22 +33,22 @@ def record_view(sell):
     return render_template('view.html', title='View Form', home=result[0])
 
 
-@app.route('/edit/<int:city_id>', methods=['GET'])
-def form_edit_get(city_id):
+@app.route('/edit/<string:sell>', methods=['GET'])
+def form_edit_get(sell):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM homes WHERE id=%s', city_id)
+    cursor.execute('SELECT * FROM homes WHERE Sell=%s', sell)
     result = cursor.fetchall()
-    return render_template('edit.html', title='Edit Form', city=result[0])
+    return render_template('edit.html', title='Edit Form', home=result[0])
 
 
-@app.route('/edit/<int:city_id>', methods=['POST'])
-def form_update_post(city_id):
+@app.route('/edit/<string:sell>', methods=['POST'])
+def form_update_post(sell):
     cursor = mysql.get_db().cursor()
-    inputData = (request.form.get('fldName'), request.form.get('fldLat'), request.form.get('fldLong'),
-                 request.form.get('fldCountry'), request.form.get('fldAbbreviation'),
-                 request.form.get('fldCapitalStatus'), request.form.get('fldPopulation'), city_id)
-    sql_update_query = """UPDATE homes t SET t.fldName = %s, t.fldLat = %s, t.fldLong = %s, t.fldCountry = 
-    %s, t.fldAbbreviation = %s, t.fldCapitalStatus = %s, t.fldPopulation = %s WHERE t.id = %s """
+    inputData = (request.form.get('Sell'), request.form.get('List'), request.form.get('Living'),
+                 request.form.get('Rooms'), request.form.get('Beds'), request.form.get('Baths'),
+                 request.form.get('Age'), request.form.get('Acres'), request.form.get('Taxes'), sell)
+    sql_update_query = """UPDATE homes t SET t.Sell = %s, t.List = %s, t.Living = %s, t.Rooms = 
+    %s, t.Beds = %s, t.Baths = %s, t.Age = %s, t.Acres = %s, t.Taxes = %s WHERE t.Sell = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
@@ -64,16 +64,16 @@ def form_insert_post():
     inputData = (request.form.get('Sell'), request.form.get('List'), request.form.get('Living'),
                  request.form.get('Rooms'), request.form.get('Beds'), request.form.get('Baths'),
                  request.form.get('Age'), request.form.get('Acres'), request.form.get('Taxes'))
-    sql_insert_query = """INSERT INTO homes (Sell,List,Living,Rooms,Beds,Baths,Age,Acres,Taxes) VALUES (%s,%d,%d,%d,%d,%d,%d,%d,%d) """
+    sql_insert_query = """INSERT INTO homes (Sell,List,Living,Rooms,Beds,Baths,Age,Acres,Taxes) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
-@app.route('/delete/<int:city_id>', methods=['POST'])
-def form_delete_post(city_id):
+@app.route('/delete/<string:sell>', methods=['POST'])
+def form_delete_post(sell):
     cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM homes WHERE id = %s """
-    cursor.execute(sql_delete_query, city_id)
+    sql_delete_query = """DELETE FROM homes WHERE Sell = %s """
+    cursor.execute(sql_delete_query, sell)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
